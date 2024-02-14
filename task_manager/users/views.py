5ff0a1e03cef0e272090beb5bd9432be4299d9c7
alllexxx1 <alekkorpachyov@gmail.com>
2024-02-14 12:11:56 +0300

    Override editing and deletion views to generic UpadateView and DeleteView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.views.generic import ListView, CreateView, View
+from django.views.generic import ListView, CreateView, UpdateView, View, DeleteView
 from django.utils.translation import gettext as _
 from task_manager.users.models import User
 from task_manager.users.forms import UserCreateForm
@@ -21,47 +21,35 @@ class CreateUserView(SuccessMessageMixin, CreateView):
     success_message = _('User has been successfully registered')
 
 
-class UpdateUserView(View):
-
-    def get(self, request, *args, **kwargs):
-        user_pk = kwargs.get('pk')
-        user = User.objects.get(pk=user_pk)
-        form = UserCreateForm(instance=user)
-        return render(
-            request,
-            'users/update.html',
-            {'form': form, 'user_pk': user_pk}
-        )
-
-    def post(self, request, *args, **kwargs):
-        user_pk = kwargs.get('pk')
-        user = User.objects.get(pk=user_pk)
-        form = UserCreateForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, _('User has been successfully changed'))
-            return redirect('users:users')
-        return render(
-            request,
-            'users/update.html',
-            {'form': form, 'user_pk': user_pk}
-        )
+class UpdateUserView(SuccessMessageMixin, UpdateView):
+    model = User
+    form_class = UserCreateForm
+    template_name = 'users/update.html'
+    success_url = reverse_lazy('users:users')
+    success_message = _('User has been successfully changed')
 
 
-class DeleteUserView(View):
-    def get(self, request, *args, **kwargs):
-        user_pk = kwargs.get('pk')
-        user = User.objects.get(pk=user_pk)
-        return render(
-            request,
-            'users/delete.html',
-            {'user': user}
-        )
+class DeleteUserView(SuccessMessageMixin, DeleteView):
+    model = User
+    template_name = 'users/delete.html'
+    success_url = reverse_lazy('users:users')
+    success_message = _('User has been successfully deleted')
 
-    def post(self, request, *args, **kwargs):
-        user_pk = kwargs.get('pk')
-        user = User.objects.get(pk=user_pk)
-        if user:
-            user.delete()
-            messages.success(request, _('User has been successfully deleted'))
-        return redirect('users:users')
+
+# class DeleteUserView(View):
+#     def get(self, request, *args, **kwargs):
+#         user_pk = kwargs.get('pk')
+#         user = User.objects.get(pk=user_pk)
+#         return render(
+#             request,
+#             'users/delete.html',
+#             {'user': user}
+#         )
+#
+#     def post(self, request, *args, **kwargs):
+#         user_pk = kwargs.get('pk')
+#         user = User.objects.get(pk=user_pk)
+#         if user:
+#             user.delete()
+#             messages.success(request, _('User has been successfully deleted'))
+#         return redirect('users:users')
