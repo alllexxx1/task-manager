53ca3d4fa7d4994d81b3b598e98d8c6f3d6aa241
alllexxx1 <alekkorpachyov@gmail.com>
2024-02-16 16:42:45 +0300

    Override atribute ordering in UserView to get a proper list of Users

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -14,6 +14,7 @@ class UsersView(ListView):
     model = User
     template_name = 'users/users.html'
     context_object_name = 'users'
+    ordering = ['id']
 
 
 class CreateUserView(SuccessMessageMixin, CreateView):
