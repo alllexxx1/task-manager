29b67811c9222796915e6930cfa5f407ecfb0ff2
alllexxx1 <alekkorpachyov@gmail.com>
2024-02-19 16:54:06 +0300

    Create a base structure of a labels app and full set of tests for CRUD operations

---
+++ task_manager/labels/views.py
@@ -0,0 +1,22 @@
+# from django.views.generic import (
+#     ListView, CreateView,
+#     UpdateView, DeleteView)
+# from django.contrib.messages.views import SuccessMessageMixin
+# from django.contrib import messages
+# from task_manager.custom_utils import AuthRequiredMixin, DeletionProtectHandleMixin
+#
+#
+# class LabelsView(ListView):
+#     pass
+#
+#
+# class CreateLabelView(CreateView):
+#     pass
+#
+#
+# class UpdateLabelView(UpdateView):
+#     pass
+#
+#
+# class DeleteLabelView(DeleteView):
+#     pass
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'task_manager.users',
     'task_manager.statuses',
     'task_manager.tasks',
+    'task_manager.labels',
     'django_extensions',
     'django_filters',
 ]
