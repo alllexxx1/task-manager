04209b9b028cdec27b8d077bc633af021b473dd7
alllexxx1 <alekkorpachyov@gmail.com>
2024-02-23 21:00:22 +0300

    Remove icons from Sign-in and sign-up buttons

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -4,6 +4,13 @@ from django.urls import reverse
 
 class ChangeLanguageTestCase(TestCase):
 
-    def test_language_using_header(self):
+    def test_various_language_using_header(self):
         response = self.client.get(reverse('index'), HTTP_ACCEPT_LANGUAGE='ru')
         self.assertContains(response, 'Узнать больше')
+
+        response = self.client.get(reverse('index'), HTTP_ACCEPT_LANGUAGE='en')
+        self.assertContains(response, 'Learn more...')
+
+    def test_test(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, 'Вход')
