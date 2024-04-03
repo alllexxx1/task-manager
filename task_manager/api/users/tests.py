3c673e4d700a63ef9c83f6984f77f92e0fdf51be
alllexxx1 <alekkorpachyov@gmail.com>
2024-04-03 20:36:33 +0300

    Add documentation for API test cases

--- task_manager/api/users/tests.py
+++ task_manager/api/users/tests.py
@@ -10,9 +10,19 @@ from task_manager.users.tests import load_fixture
 
 
 class UserAPITestCase(APITestCase):
+    """
+    Test case for the UserAPIView.
+
+    This test case ensures that the API endpoint returns
+    the expected list of users in the correct format.
+    """
+
     fixtures = ['users.json']
 
     def test_get_users(self):
+        """
+        Test the retrieval of user data through the API.
+        """
         url = reverse('user-list')
         response = self.client.get(url)
         users = get_user_model().objects.all()
@@ -24,9 +34,21 @@ class UserAPITestCase(APITestCase):
 
 
 class UserSerializerTestCase(TestCase):
+    """
+    Test case for the UserSerializer.
+
+    This test case verifies the serialization process
+    of the UserSerializer, ensuring that the serialized
+    data matches the expected format.
+    """
+
     fixtures = ['users.json']
 
     def test_serializer(self):
+        """
+        Verify that the UserSerializer correctly
+        serializes user data into JSON format.
+        """
         user = get_user_model().objects.get(id=1)
         serialized_data = UserSerializer(user).data
         expected_data = load_fixture('serialized_users.json')
